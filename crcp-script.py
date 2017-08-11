99cf29ea41a99db9cb4beb4383f86919640021d3
Oscar Lai <tlai4178@uni.sydney.edu.au>
2017-08-11 11:53:13 +1000

    added title for each script sections

--- crcp-script.py
+++ crcp-script.py
@@ -13,26 +13,26 @@ from sketch import *
 from visualization import *
 from connectorBehavior import *
 
-# MDOEL SETTINGS
+##################################################
+##### MDOEL SETTINGS
+##################################################
 model_name = 'CRCP'
 model_width = 1524.0
 model_height = 304.8
 model_depth = 1828.0
-
-
 partition_size = 38.1
 
 # delete existing if exists
 if model_name in mdb.models.keys():
     del mdb.models[model_name]
 mdb.Model(modelType=STANDARD_EXPLICIT, name=model_name)
+
 mdl = mdb.models[model_name]
 
 
-
-#####################################################
-### Setup concrete model dimension PART
-#####################################################
+##################################################
+##### SETUP PART DIMENSIONS
+##################################################
 # Concrete
 mdl.ConstrainedSketch(name='__profile__', sheetSize=3000.0)
 mdl.sketches['__profile__'].rectangle(point1=(0.0, 0.0),
@@ -60,12 +60,20 @@ mdl.Part(dimensionality=THREE_D, name='trSteelBarPart', type=
 mdl.parts['trSteelBarPart'].BaseWire(sketch=
     mdl.sketches['__profile__'])
 del mdl.sketches['__profile__']
+#### Creating Wheel
+mdl.ConstrainedSketch(name='__profile__', sheetSize=1000.0)
+mdl.sketches['__profile__'].CircleByCenterPerimeter(center=(
+    0.0, 0.0), point1=(0.0, 150.0))
+mdl.Part(dimensionality=THREE_D, name='wheelPart', type=
+    DEFORMABLE_BODY)
+mdl.parts['wheelPart'].BaseSolidExtrude(depth=100.0, sketch=
+    mdl.sketches['__profile__'])
+del mdl.sketches['__profile__']
 
 
-
-#####################################################
-### Setup Material Property
-#####################################################
+##################################################
+##### SETUP MATERIAL PROPERTIES
+##################################################
 # Concrete
 mdl.Material(name='Concrete')
 mdl.materials['Concrete'].Elastic(table=((13780.0, 0.15), ))
@@ -82,10 +90,9 @@ mdl.materials['TrSteel'].Elastic(table=((200000.0, 0.0), ))
 mdl.materials['TrSteel'].Expansion(table=((1.08e-05, ), ))
 
 
-
-#####################################################
-### Create Instances
-#####################################################
+##################################################
+##### CREATE INSTANCES
+##################################################
 # Create Instance
 mdl.rootAssembly.DatumCsysByDefault(CARTESIAN)
 mdl.rootAssembly.Instance(dependent=ON, name='concslab',
@@ -94,9 +101,12 @@ mdl.rootAssembly.Instance(dependent=ON, name='sbar', part=
     mdl.parts['steelbarPart'])
 mdl.rootAssembly.Instance(dependent=ON, name='trsbar',
     part=mdl.parts['trSteelBarPart'])
+mdl.rootAssembly.Instance(dependent=ON, name='wheel-1',
+    part=mdl.parts['wheelPart'])
 
-##### Create each Steel Bar instances at correct pos
-
+##################################################
+##### TRANSLATE INSTANCES TO CORRECT POS
+##################################################
 ### Lognitudial steel bar
 mdl.rootAssembly.translate(instanceList=('sbar', ),
     vector=(0.0, 152.4, 76.2))
@@ -117,7 +127,6 @@ mdl.rootAssembly.features.changeKey(fromName='sbar-lin-1-9', toName='sbar9')
 mdl.rootAssembly.features.changeKey(fromName='sbar-lin-1-10', toName='sbar10')
 mdl.rootAssembly.features.changeKey(fromName='sbar-lin-1-11', toName='sbar11')
 mdl.rootAssembly.features.changeKey(fromName='sbar-lin-1-12', toName='sbar12')
-
 ### Transverse steel bar
 mdl.rootAssembly.instances['trsbar'].translate(vector=(
     1706.88, 0.0, 0.0))
@@ -133,13 +142,16 @@ mdl.rootAssembly.LinearInstancePattern(direction1=(-1.0, 0.0,
 mdl.rootAssembly.features.changeKey(fromName='trsbar', toName='trsbar1')
 mdl.rootAssembly.features.changeKey(fromName='trsbar-lin-2-1', toName='trsbar2')
 
+#### translate wheel
+mdl.rootAssembly.translate(instanceList=('Wheel-1', ), vector=(
+    323.85, 454.8, -61.9))
+mdl.rootAssembly.rotate(angle=90.0, axisDirection=(0.0, 150.0,
+    0.0), axisPoint=(323.85, 304.8, 38.1), instanceList=('Wheel-1', ))
 
 
-
-
-
-
-### Create datum plane
+##################################################
+##### CREATE DATUM PLANE FOR PARTITIONS
+##################################################
 for i in range(48-1):
 	mdl.parts['concslabPart'].DatumPlaneByPrincipalPlane(offset=partition_size * (i+1)
 		, principalPlane=XYPLANE)
@@ -154,6 +166,41 @@ for i in range(8-1):
 for k,v in mdl.parts['concslabPart'].datums.items():
 	mdl.parts['concslabPart'].PartitionCellByDatumPlane(cells=
 		mdl.parts['concslabPart'].cells, datumPlane=v)
+
+## Partioning Longitudinal and Transverse steel bar in Part
+for i in range(48-1):
+	mdl.parts['trSteelBarPart'].DatumPointByCoordinate(coords=(
+		partition_size*(i+1), 0.0, 0.0))
+for i in range(40-1):
+	mdl.parts['steelbarPart'].DatumPointByCoordinate(coords=(
+		partition_size*(i+1), 0.0, 0.0))
+	## Partitioning the steel bar
+for i in range(40-1):
+	mdl.parts['steelbarPart'].PartitionEdgeByPoint(edge=
+    mdl.parts['steelbarPart'].edges[i], point=
+    mdl.parts['steelbarPart'].datums[i+2])
+for i in range(48-1):
+	mdl.parts['trSteelBarPart'].PartitionEdgeByPoint(edge=
+    mdl.parts['trSteelBarPart'].edges[i], point=
+    mdl.parts['trSteelBarPart'].datums[i+2])
+
+
+##################################################
+##### CONNECT STEEL BAR TO CONCRETE
+##################################################
+# iterate all steel bar to get its vertices and connect to concrete slab
+stbarInstances = []
+for i in mdl.rootAssembly.instances.keys():
+	if 'sbar' in i:
+		stbarInstances.append(i)
+
+for stbar in stbarInstances:
+	vertices = mdl.rootAssembly.instances[stbar].vertices
+	for stbarV in vertices:
+		concV = mdl.rootAssembly.instances['concslab'].vertices.findAt(stbarV.pointOn[0])
+		# connect these two point
+		mdl.rootAssembly.WirePolyLine(mergeType=IMPRINT, meshable=OFF
+			, points=((stbarV, concV), ))
 
 
 ## Define Connector behavior
@@ -175,78 +222,38 @@ mdl.sections['NonLinear-BondSp edge'].setValues(
     212.05708, 0.1016), (0.0, 0.2032)), independentComponents=(), components=(
     1, )), ))
 
-## Partioning Longitudinal and Transverse steel bar in Part
-	##Creating DatumPointBy
 
-for i in range(48-1):
-	mdl.parts['trSteelBarPart'].DatumPointByCoordinate(coords=(
-		partition_size*(i+1), 0.0, 0.0))
-for i in range(40-1):
-	mdl.parts['steelbarPart'].DatumPointByCoordinate(coords=(
-		partition_size*(i+1), 0.0, 0.0))
-	## Partitioning the steel bar
-for i in range(40-1):
-	mdl.parts['steelbarPart'].PartitionEdgeByPoint(edge=
-    mdl.parts['steelbarPart'].edges[i], point=
-    mdl.parts['steelbarPart'].datums[i+2])
-for i in range(48-1):
-	mdl.parts['trSteelBarPart'].PartitionEdgeByPoint(edge=
-    mdl.parts['trSteelBarPart'].edges[i], point=
-    mdl.parts['trSteelBarPart'].datums[i+2])
-
-
-############# Connect steel bar with concrete
-
-# iterate all steel bar to get its vertices and connect to concrete slab
-stbarInstances = []
-for i in mdl.rootAssembly.instances.keys():
-	if 'sbar' in i:
-		stbarInstances.append(i)
-
-for stbar in stbarInstances:
-	vertices = mdl.rootAssembly.instances[stbar].vertices
-	for stbarV in vertices:
-		concV = mdl.rootAssembly.instances['concslab'].vertices.findAt(stbarV.pointOn[0])
-
-		# connect these two point
-		mdl.rootAssembly.WirePolyLine(mergeType=IMPRINT, meshable=OFF
-			, points=((stbarV, concV), ))
-
-
+##################################################
+##### CONNECT CONCRETE AND ITS SHEAR LAYER UNDERNEATH
+##################################################
 ### shear layer connector section properties
-
 mdl.ConnectorSection(name='Conc and base friction-interior',
     translationalType=CARTESIAN)
 mdl.sections['Conc and base friction-interior'].setValues(
     behaviorOptions=(ConnectorElasticity(table=((59.080527, 59.080527), ),
     independentComponents=(), components=(1, 2)), ))
-
 mdl.ConnectorSection(name='Conc and base friction-edge',
     translationalType=CARTESIAN)
 mdl.sections['Conc and base friction-edge'].setValues(
     behaviorOptions=(ConnectorElasticity(table=((29.5402635, 29.5402635), ),
     independentComponents=(), components=(1, 2)), ))
-
 mdl.ConnectorSection(name='Conc and base friction-corner',
     translationalType=CARTESIAN)
 mdl.sections['Conc and base friction-corner'].setValues(
     behaviorOptions=(ConnectorElasticity(table=((14.77013175, 14.77013175), ),
     independentComponents=(), components=(1, 2)), ))
 
-
-
 ############# Connect concrete slab with shear layer
 # iterate all concrete slab vertices and only apply connector for the one with constraints y=0
-
 vertices = mdl.rootAssembly.instances['concslab'].vertices
-
 for v in vertices:
 	if int(v.pointOn[0][1]) == 0: # at y=0
 		mdl.rootAssembly.WirePolyLine(mergeType=IMPRINT, meshable=OFF
 			, points=((None, v), ))
 
-
-########## Defining node sets
+##################################################
+##### DEFINE SETS (NODES & SURFACES)
+##################################################
 #### make each surfacec in y axis as node set
 lvl = model_height
 j = 0
@@ -277,7 +284,6 @@ mdl.rootAssembly.Set(name='sbarNodeSet_zMin', vertices=vertices)
 vertices = mdl.rootAssembly.instances['trsbar'].vertices.getByBoundingBox(zMax=model_depth, zMin=model_depth)
 mdl.rootAssembly.Set(name='sbarNodeSet_zMax', vertices=vertices)
 
-
 ####### Create Surface set on all six faces
 faces = mdl.rootAssembly.instances['concslab'].faces.getByBoundingBox(xMax=0, xMin=0)
 mdl.rootAssembly.Set(name='SurfaceSet_xMin', faces=faces)
@@ -292,8 +298,9 @@ mdl.rootAssembly.Set(name='SurfaceSet_zMin', faces=faces)
 faces = mdl.rootAssembly.instances['concslab'].faces.getByBoundingBox(zMax=model_depth, zMin=model_depth)
 mdl.rootAssembly.Set(name='SurfaceSet_zMax', faces=faces)
 
-
-# boundary Condition
+##################################################
+##### BOUNDARY CONDITION
+##################################################
 # conc
 mdl.DisplacementBC(amplitude=UNSET, createStepName='Initial',
     distributionType=UNIFORM, fieldName='', localCsys=None, name='FrontBC',
@@ -318,29 +325,9 @@ mdl.DisplacementBC(amplitude=UNSET, createStepName='Initial',
     UNSET, u3=SET, ur1=SET, ur2=UNSET, ur3=SET)
 
 
-#### Creating Wheel
-mdl.ConstrainedSketch(name='__profile__', sheetSize=1000.0)
-mdl.sketches['__profile__'].CircleByCenterPerimeter(center=(
-    0.0, 0.0), point1=(0.0, 150.0))
-mdl.Part(dimensionality=THREE_D, name='WheelPart', type=
-    DEFORMABLE_BODY)
-mdl.parts['WheelPart'].BaseSolidExtrude(depth=100.0, sketch=
-    mdl.sketches['__profile__'])
-del mdl.sketches['__profile__']
-
-### instance of wheels
-mdl.rootAssembly.Instance(dependent=ON, name='Wheel-1',
-    part=mdl.parts['WheelPart'])
-
-# translate wheel
-mdl.rootAssembly.translate(instanceList=('Wheel-1', ), vector=(
-    323.85, 454.8, -61.9))
-mdl.rootAssembly.rotate(angle=90.0, axisDirection=(0.0, 150.0,
-    0.0), axisPoint=(323.85, 304.8, 38.1), instanceList=('Wheel-1', ))
-
 # Define contact surfaces
 mdl.rootAssembly.Surface(name='WheelSurface', side1Faces=
-    mdl.rootAssembly.instances['Wheel-1'].faces.getSequenceFromMask(
+    mdl.rootAssembly.instances['wheel-1'].faces.getSequenceFromMask(
     ('[#1 ]', ), ))
 mdl.rootAssembly.Surface(name='TopSurface', side1Faces=
     mdl.rootAssembly.sets['SurfaceSet_yMax'].faces)
@@ -358,25 +345,25 @@ mdl.SurfaceToSurfaceContactExp(clearanceRegion=None,
     mdl.rootAssembly.surfaces['TopSurface'],
     mechanicalConstraint=KINEMATIC, name='Int-1', slave=
     mdl.rootAssembly.surfaces['WheelSurface'], sliding=FINITE)
-mdl.parts['WheelPart'].ReferencePoint(point=
-    mdl.parts['WheelPart'].InterestingPoint(
-    mdl.parts['WheelPart'].edges[0], CENTER))
+mdl.parts['wheelPart'].ReferencePoint(point=
+    mdl.parts['wheelPart'].InterestingPoint(
+    mdl.parts['wheelPart'].edges[0], CENTER))
 mdl.Velocity(distributionType=MAGNITUDE, field='', name=
     'Predefined Field-1', omega=0.0, region=Region(
-    cells=mdl.rootAssembly.instances['Wheel-1'].cells.getSequenceFromMask(
+    cells=mdl.rootAssembly.instances['wheel-1'].cells.getSequenceFromMask(
     mask=('[#1 ]', ), ),
-    faces=mdl.rootAssembly.instances['Wheel-1'].faces.getSequenceFromMask(
+    faces=mdl.rootAssembly.instances['wheel-1'].faces.getSequenceFromMask(
     mask=('[#7 ]', ), ),
-    edges=mdl.rootAssembly.instances['Wheel-1'].edges.getSequenceFromMask(
+    edges=mdl.rootAssembly.instances['wheel-1'].edges.getSequenceFromMask(
     mask=('[#3 ]', ), ),
-    vertices=mdl.rootAssembly.instances['Wheel-1'].vertices.getSequenceFromMask(
+    vertices=mdl.rootAssembly.instances['wheel-1'].vertices.getSequenceFromMask(
     mask=('[#3 ]', ), ), referencePoints=(
-    mdl.rootAssembly.instances['Wheel-1'].referencePoints[2], ))
+    mdl.rootAssembly.instances['wheel-1'].referencePoints[2], ))
     , velocity1=0.0, velocity2=0.0, velocity3=10.0)
 mdl.DisplacementBC(amplitude=UNSET, createStepName='Initial',
     distributionType=UNIFORM, fieldName='', localCsys=None, name='BC-2',
     region=Region(referencePoints=(
-    mdl.rootAssembly.instances['Wheel-1'].referencePoints[2], ))
+    mdl.rootAssembly.instances['wheel-1'].referencePoints[2], ))
     , u1=SET, u2=SET, u3=SET, ur1=UNSET, ur2=SET, ur3=SET)
 mdl.ExplicitDynamicsStep(massScaling=((SEMI_AUTOMATIC, MODEL,
     AT_BEGINNING, 0.0, 5.55e-07, BELOW_MIN, 0, 0, 0.0, 0.0, 0, None), ), name=
@@ -384,7 +371,7 @@ mdl.ExplicitDynamicsStep(massScaling=((SEMI_AUTOMATIC, MODEL,
 mdl.VelocityBC(amplitude=UNSET, createStepName='Step-1',
     distributionType=UNIFORM, fieldName='', localCsys=None, name='BC-3',
     region=Region(referencePoints=(
-    mdl.rootAssembly.instances['Wheel-1'].referencePoints[2], ))
+    mdl.rootAssembly.instances['wheel-1'].referencePoints[2], ))
     , v1=UNSET, v2=UNSET, v3=UNSET, vr1=-5.0, vr2=UNSET, vr3=UNSET)
 
 
